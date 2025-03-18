f05b8afeb378df80d943f57a5c1ac35f29400019
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-03-18 23:58:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 26537149
 api_hash = "2697a63a384fd9b95520ee63530f6a75"
-bot_token = "7492339838:AAFZDkDEL-NBQ6jkY3PbMrDLUQcrTrzg_kc"
+bot_token = ""
 auth_users = [7991268498]
 sudo_user = 7991268498
 log_channel = -1002442546983
